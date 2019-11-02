33903de97e025df3e8c49afa9362a80a2d2b98a7
koko0215 <2018120215a@gmail.com>
2019-11-02 14:43:38 +0900

    »これからの基本実装

--- hoge.cpp
+++ hoge.cpp
@@ -1,9 +1,8 @@
-#include<iostream>
-#include"format.hpp"
-#include<tuple>
-int main(){
+#include <iostream>
+#include "format.hpp"
+#include <tuple>
+int main()
+{
     Koko koko;
-    std::tuple<std::string, int, double> hoge = koko.format("hoge",114,5.14);
-    std::string tuple_1 = std::get<0>(hoge);
-    std::cout << tuple_1 << std::endl;
+    koko.format("ho{}ge", 10);
 }
