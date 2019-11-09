14bfb9523c141bd9a9a70cb07f338ff3a90c0e33
koko0215 <2018120215a@gmail.com>
2019-11-09 19:55:46 +0900

    例外処理の追加

--- format.hpp
+++ format.hpp
@@ -1,5 +1,6 @@
 #include <string>
 #include <iostream>
+#include <stdexcept>
 namespace koko
 {
 auto format(std::string str)
@@ -7,11 +8,11 @@ auto format(std::string str)
     return str;
 }
 template <typename Arg, typename... Args>
-auto format(std::string str, Arg arg, Args... args)
+auto format(std::string str, Arg arg = 0, Args... args)
 {
     auto result = str.find("{}");
     if (result == std::string::npos)
-        return str;
+        throw std::invalid_argument("There is no '{}' in the first argument");
     str.erase(result, 2);
     str.insert(result, std::to_string(arg));
     return format(str, args...);
