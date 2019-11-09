7a3d00e534bcf3ce6c9842b19a890c0f9d457549
koko0215 <2018120215a@gmail.com>
2019-11-09 13:42:39 +0900

    仮実装完成

--- format.hpp
+++ format.hpp
@@ -5,15 +5,18 @@
 #include <iostream>
 namespace koko
 {
+auto format(std::string str)
+{
+    return str;
+}
 template <typename Arg, typename... Args>
 auto format(std::string str, Arg arg, Args... args)
 {
     auto result = str.find("{}");
-    str.insert(result, arg);
     if (result == std::string::npos)
-    {
         return str;
-    }
-    format(str, args...);
+    str.erase(result, 2);
+    str.insert(result, std::to_string(arg));
+    return format(str, args...);
 }
 }; // namespace koko
